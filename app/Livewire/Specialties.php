d1476b2784780d09b900f709a92b71ee1cbf8fe0
Daniil Mironov <m05419325@gmail.com>
2024-11-07 17:52:06 +0300

    Сделал список в блейде специальностей

--- app/Livewire/Specialties.php
+++ app/Livewire/Specialties.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Specialty;
 use Livewire\Component;
 
 class Specialties extends Component
 {
     public function render()
     {
-        return view('livewire.specialties');
+        $specialties = Specialty::latest()->paginate();
+        return view('livewire.specialties', compact('specialties'));
     }
 }
+/*
+
+    public function delete(Question $question): void
+    {
+        abort_if(! auth()->user()->is_admin, Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $question->delete();
+    }*/
